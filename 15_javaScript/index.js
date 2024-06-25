9ba4ff2d203fbb688012a22ae1f926d765df3578
Mohd Shahbaz <Shahbaz705454@gmail.com>
2024-06-25 20:49:15 +0530

    added java script file

--- 15_javaScript/index.js
+++ 15_javaScript/index.js
@@ -4,14 +4,83 @@ console.log("hello world version 4");
 
 
 // let variable scope is local scope word under local scope under some condition
-let a =5;
-let b= "shahbaz";
-let x = 5.667;
-let c =true;
-console.log(a);
-console.log(b);
-console.log(x);
-console.log(c);
+// let a =5;
+// let b= "shahbaz";
+// let x = 5.667;
+// let c =true;
+// console.log(a);
+// console.log(b);
+// console.log(x);
+// console.log(c);
+// a=55;
+// console.log("this is next varible "+a);
+
 
 // var variable scope is globar varible type declaration
+
+var d = 24;
+console.log(d);
+d =54;
+console.log(d);
+
+
+// Constant varible are not changeible
+
+const z =46;
+
+
+// object creation 
+
+let person = {
+    
+    
+
+
+};
+
+
+// array declartaion 
+
+let arr1 =['shahbaz','aman' ,'kasif'];
+
+console.log(arr1[0]);
+console.log(arr1[1]);
+console.log(arr1[2]);
+
+for (let index = 0; index < arr1.length; index++) {
+    console.log(arr1[index])
+    
+}
+
+
+// incereament operator 
+
+let xy = 7;
+console.log(xy++);
+console.log(++xy);
+
+let yx =9;
+console.log(yx--);
+console.log(--yx);
+
+
+// condition 
+let a = 10;
+let b = 6;
+let c = 10;
+
+if(a===c || b>10){
+    console.log('true');
+}else{
+    console.log('false');
+}
+
+if(a===c && b<10){
+    console.log('true');
+}else{
+    console.log('false');
+}
+
+
+
 
