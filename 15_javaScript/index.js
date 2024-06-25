e32246ef30d72bdc2b04f0da3d57b2e476104cc7
Mohd Shahbaz <Shahbaz705454@gmail.com>
2024-06-25 12:10:00 +0530

    new java scrpt file added

---
+++ 15_javaScript/index.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+// alert("hello");
+console.log("hello world version 4");
+
+
+// let variable scope is local scope word under local scope under some condition
+let a =5;
+let b= "shahbaz";
+let x = 5.667;
+let c =true;
+console.log(a);
+console.log(b);
+console.log(x);
+console.log(c);
+
+// var variable scope is globar varible type declaration
+
