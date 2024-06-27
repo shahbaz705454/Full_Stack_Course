d7433174377f8614a5be334fb9649f11d81198f1
Mohd Shahbaz <Shahbaz705454@gmail.com>
2024-06-27 11:30:12 +0530

    added array concept

--- 17_javaScript_Arrays/index.js
+++ 17_javaScript_Arrays/index.js
@@ -2,12 +2,12 @@ Math.random();
 console.log(Math.random())
 console.log(Math.round(1.8))
 console.log(Math.round(1.4))
-console.log(Math.max(1,4,5,6,3,4,7,9,4,6))
+console.log(Math.max(1, 4, 5, 6, 3, 4, 7, 9, 4, 6))
 
 
 // String
 
-let name="Shahbaz";
+let name = "Shahbaz";
 console.log(name.length);
 console.log(name.includes('Sh'));
 console.log(name.startsWith('Shah'));
@@ -18,10 +18,65 @@ console.log(name.trim());
 console.log(name.repeat(3));
 
 
-let message ="This is my first message";
-let word= message.split(" ");
+let message = "This is my first message";
+let word = message.split(" ");
 console.log(word);
 for (const key in word) {
     console.log(key + " " + word[key]);
 }
 
+let design = `This is my first
+messahe and this is back tick function that 
+allow your string to desin in any
+       way`;
+
+console.log(design);
+let names = "shahbaz";
+
+
+let messages = ` hello my name is ${names},
+                  good morning everyone`;
+
+
+console.log(messages);
+
+// date
+let date = new Date();
+console.log(date);
+
+let date2 = new Date('Aug 28 2003');
+console.log(date2);
+
+let date3 = new Date()
+
+console.log(new Date(8.64e15).toString());
+
+let date4 = new Date(2003, 7, 28, 7);
+console.log(date4.getTime);
+
+
+
+// ARRAYS
+
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (const key in arr1) {
+
+    console.log("index : " + key + " element :" + arr1[key]);
+}
+
+console.log(arr1);
+
+
+// INSERTION
+
+// at end
+arr1.push(10);
+
+// at begin
+arr1.unshift(11);
+
+// at midlle
+arr1.splice(5,0,'a');
+
+console.log(arr1);
