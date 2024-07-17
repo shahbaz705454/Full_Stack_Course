2f715e68aaec8f34e5970ff80778a1e2a966bd87
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-07-17 21:13:59 +0530

    added

--- 19_javaScript_Dom_Eventlistner/index.js
+++ 19_javaScript_Dom_Eventlistner/index.js
@@ -1,4 +1,32 @@
-document.addEventListener('click', function(){
-    console.log("this is event lsitner call");
+// EVent listner is used to listen like click scroll and rect on that trigerr event
+
+
+function event() {
+    console.log("This is event  listener function ")
+    
+}
+
+
+// document.addEventListener('click', function(){
+//     console.log("this is event listener call");
+
+// })
+
+
+document.addEventListener('click', event);
+
+//---------- Remove eventListner Function is used to remove Event on that funtion
+document.removeEventListener('click',event);
+
+// =-----------------------------> PHASES OF EVENTS
+// ----->Capturing phase 
+// ------>At Target Phase
+// ------>BuBBling Phase
+
+
+const content = document.querySelector('#wrapper');
+
+content.addEventListener('click', function(event) {
+    console.log(event);
     
 })
