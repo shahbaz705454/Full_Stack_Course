6853bf09664f8b84dd65325f8006a5788de27375
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-07-18 12:53:57 +0530

    added new

--- 19_javaScript_Dom_Eventlistner/index.js
+++ 19_javaScript_Dom_Eventlistner/index.js
@@ -39,3 +39,59 @@ content.addEventListener('click', function(event) {
     event.preventDefault();
     console.log("maza aya");
  })
+
+
+
+//  funtion that create 100 p elelment with event listener
+
+// let myDiv = document.createElement('div');
+
+// function paraStatus(event){
+//     console.log("I have clicked on para");
+// }
+
+// myDiv.addEventListener('click', paraStatus);
+
+// for (let i = 1; i <=100; i++){
+//     let newElement = document.createElement('p');
+//     newElement.textContent='This is para' + 1;
+    
+
+//     myDiv.appendChild(newElement);
+   
+// }
+// document.body.appendChild(myDiv);
+
+// --------> IN the abpve function event lisner added on div so if we click any where in div event called 
+// --------> so make new target spefic event listner  
+
+// let myDiv2 = document.createElement('div');
+
+// function paraStatus(event){
+//     console.log('para' + event.target.textContent);
+// }
+
+// myDiv2.addEventListener('click', paraStatus);
+
+// for (let i = 1; i <=100; i++){
+//     let newElement2 = document.createElement('p');
+//     newElement2.textContent='This is para' + i;
+    
+
+//     myDiv2.appendChild(newElement2);
+   
+// }
+// document.body.appendChild(myDiv2);
+
+
+// CREATED svent listner on differnet elemnt on same element
+
+let element = document.querySelector('#element');
+
+element.addEventListener('click', function (event) {
+
+    // Adeed this if condition if we want to print only pan tag not para tag
+    if(event.target.nodeName==='SPAN')
+    console.log(' click kia hai ' + event.target.textContent+ " par");
+    
+});
