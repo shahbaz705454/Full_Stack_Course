c18886eda07a2864aee4729e477d94faa810c4c6
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-07-19 23:19:39 +0530

    added

--- 20_javaScript_performance/index.js
+++ 20_javaScript_performance/index.js
@@ -28,7 +28,7 @@ let fragment = document.createDocumentFragment();
 //     element.textContent= 'This is new paragraph ' + i;
 
 //     fragment.appendChild(element);
-   
+
 // }
 document.body.appendChild(fragment); // REFLOW ,REPAINT
 
@@ -67,23 +67,48 @@ document.body.appendChild(fragment); // REFLOW ,REPAINT
 
 //     setTimeout(function() {
 //         console.log("I am inside promise 1 ")
-        
+
 //     }, 5000);
 //     resolve(2233);
 //     // reject(new Error('Bhaisabh Error aaye hai'))
 // });
 
-let merapromise2 = new Promise(function(resolve,reject){
+let merapromise2 = new Promise(function (resolve, reject) {
 
-    setTimeout(function() {
+    setTimeout(function () {
         console.log("I am inside promise 2 ")
-        
+
     }, 3000);
     resolve(2233);
     // reject(new Error('Bhaisabh Error aaye hai'))
 });
 
-merapromise2.then((value) => {console.log(value)});
-merapromise2.catch((error) => {console.log(error)});
+// merapromise2.then((value) => { console.log(value) });
+// merapromise2.catch((error) => { console.log(error) });
+
+//                        OR
+
+merapromise2.then((value) => { console.log(value) }) ,(error) => { console.log(error) };
 
 console.log('Pehla');
+
+
+
+
+
+//    ASYNCHRONOUS api 
+let waada1 = new Promise (function(resolve,reject){
+    setTimeout(() => {
+        console.log('Set timeout Started ');
+    }, 2000);
+    resolve(true)
+});
+
+waada1.then(()=>{
+    let waada2= new Promise(function(resolve,reject){
+        resolve("waada 2 resolved ");
+    })
+    return waada2;
+}).then((value)=>console.log(value));
+
+
