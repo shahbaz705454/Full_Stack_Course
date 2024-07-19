f28bb6b20f0faa91217c72efc4cfd82a520ba955
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-07-19 19:51:25 +0530

    ass

--- 20_javaScript_performance/index.js
+++ 20_javaScript_performance/index.js
@@ -5,17 +5,53 @@
 // 3.Event Loop
 
 //Adding 100 para
-let t1 = performance.now();
+// let t1 = performance.now();
 
-let mydiv = document.createElement('div');
-for (let i = 1 ; i <=100  ; i++) {
-    let newElement= document.createElement('p');
-    newElement.textContent="This is para " + i;
-    mydiv.appendChild(newElement);
-    
-    
+// let mydiv = document.createElement('div');
+// for (let i = 1 ; i <=100  ; i++) {
+//     let newElement= document.createElement('p');
+//     newElement.textContent="This is para " + i;
+//     mydiv.appendChild(newElement);
+// }
+
+// document.body.appendChild(mydiv);
+
+// const t2 = performance.now();
+// console.log('this took ' + (t2-t1) + 'ms');
+
+// -----------------------> FRAGMENTATION ------------------->
+
+let fragment = document.createDocumentFragment();
+
+for (let i = 0; i <= 30; i++) {
+    let element = document.createElement('p');
+    element.textContent= 'This is new paragraph ' + i;
+
+    fragment.appendChild(element);
+   
 }
-document.body.appendChild(mydiv);
+document.body.appendChild(fragment); // REFLOW ,REPAINT
 
-const t2 = performance.now();
-console.log('this took ' + (t2-t1) + 'ms');
+
+
+
+
+// -------------------  The Call Statck ------------------------->
+
+// A CALL STACK MAKE TRACK OF FUNCTION CALLS  
+
+
+
+
+// ----------------------------EVENT LOOP ---------------------------->
+// Synchronous->occuring at same time
+
+
+
+// ------------------->  TIME OUT LOOP------------------
+
+setTimeout(function timeout(){
+    console.log("welcome back")
+},5000);
+
+setTimeout();
