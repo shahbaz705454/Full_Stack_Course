4f6759d4594af47b3c6d833d8e73eb85708c00a2
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-07-20 14:02:27 +0530

    added new api fucntiuons

--- 20_javaScript_performance/index.js
+++ 20_javaScript_performance/index.js
@@ -88,7 +88,7 @@ let merapromise2 = new Promise(function (resolve, reject) {
 
 //                        OR
 
-merapromise2.then((value) => { console.log(value) }) ,(error) => { console.log(error) };
+merapromise2.then((value) => { console.log(value) }), (error) => { console.log(error) };
 
 console.log('Pehla');
 
@@ -97,20 +97,101 @@ console.log('Pehla');
 
 
 //    ASYNCHRONOUS api 
-let waada1 = new Promise (function(resolve,reject){
+let waada1 = new Promise(function (resolve, reject) {
     setTimeout(() => {
         console.log('Set timeout Started ');
     }, 2000);
     resolve(true)
-}); 
+});
 
-waada1.then(()=>{
-    let waada2= new Promise(function(resolve,reject){
+waada1.then(() => {
+    let waada2 = new Promise(function (resolve, reject) {
         resolve("waada 2 resolved ");
     })
     return waada2;
-}).then((value)=>console.log(value));
+}).then((value) => console.log(value));
 
 
 // ASYNC-AWAIT
 // Special syntex used to word with promises for multiple functions
+
+async function weatherUtility() {
+    let delhiMausam = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('delhi me bhot garmi hai')
+
+        }, 1000);
+    });
+
+    let hydMausam = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("hydrabad mausam isa cool ");
+        }, 2000);
+    });
+
+    let upMausam = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('up me barish ho rhi hai');
+        }, 3000);
+    });
+
+    let dM = delhiMausam;
+    let hM = hydMausam;
+    let upM = upMausam;
+
+    return [dM, hM, upM];
+
+}
+
+//-----------------------------> FETCH API <---------------------
+// send-->network
+// retrieve<--network 
+
+
+//   Get APi 
+// async function utility() {
+
+
+//     let content = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+//     let output = await content.json();
+//     console.log(output);
+
+// };
+
+// utility();
+
+
+// POST API
+
+async function helper() {
+
+
+
+    let options = {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'shahbaz',
+            body: 'acchi body',
+            userId: 2003,
+            weight : 70,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    };
+
+    let content2 = await fetch('https://jsonplaceholder.typicode.com/posts', options);
+    let response = await content2.json();
+
+    return response;
+
+}
+
+async function utility2() {
+
+    let ans =await helper();
+    console.log(ans);
+
+}
+
+utility2();
