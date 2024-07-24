9d27e21980c6eb25c384919e407599e20609d468
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-07-24 19:24:00 +0530

    completed

--- 23_JS_TIc_TAC_Toe_Project/script.js
+++ 23_JS_TIc_TAC_Toe_Project/script.js
@@ -1,72 +1,102 @@
 const boxes = document.querySelectorAll(".box");
-const gameinfo = document.querySelector(".game-info");
-const newGameBtn = document.querySelector(".btn");
-
-
+const gameInfo = document.querySelector(".game-info");
+const newGamebtn = document.querySelector(".btn");
+// Decide the current Turn
 let currentPlayer;
-let gameGrid; 
-const winningPosition = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-
+// All possible combinations for winning
+const winningPostions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
+let gameGrid;
 
-
-// let initialized the game
-
-function initGame(){
-    currentPlayer = "X";
-    gameGrid = ["","","","","","","","",""];
-    newGameBtn.classList.remove("active");
-    gameinfo.innerText = `Current Player -${currentPlayer} `;
-
+// Start The Game
+function initGame() {
+  currentPlayer = "x";
+  gameGrid = ["", "", "", "", "", "", "", "", ""];
+  boxes.forEach((box, index) => {
+    box.innerText = "";
+    box.style.pointerEvents = "all";
+    box.classList = `box box-${index + 1}`;
+  });
+  newGamebtn.classList.remove("active");
+  gameInfo.innerText = `Current Player - ${currentPlayer.toUpperCase()}`;
 }
 
-initGame();
-function swapTurn(){
-    if(currentPlayer=="X"){
-        currentPlayer ="O";
+// Switch player turn on click
+function swapTurn() {
+  if (currentPlayer === "x") {
+    currentPlayer = "o";
+  } else {
+    currentPlayer = "x";
+  }
+  gameInfo.innerText = `Current Player - ${currentPlayer.toUpperCase()}`;
+}
 
-    }else{
-        currentPlayer = "X";
+// Event listener
+function handleClick(index) {
+  // Make sure only, Empty cells are filled
+  if (gameGrid[index] === "") {
+    boxes[index].style.pointerEvents = "none";
+    boxes[index].innerHTML = currentPlayer.toUpperCase();
+    gameGrid[index] = currentPlayer;
+    swapTurn();
+    checkGameOver();
+  }
+}
+
+// Check if game is over
+function checkGameOver() {
+  let result = "";
+  winningPostions.forEach((position) => {
+    if (
+      (gameGrid[position[0]] !== "" ||
+        gameGrid[position[1]] !== "" ||
+        gameGrid[position[2]] !== "") &&
+      gameGrid[position[0]] === gameGrid[position[1]] &&
+      gameGrid[position[0]] === gameGrid[position[2]]
+    ) {
+      boxes.forEach((box) => {
+        box.style.pointerEvents = "none";
+      });
+      if (gameGrid[position[0]] === "x") result = "X";
+      else result = "Y";
+      boxes[position[0]].classList.add("win");
+      boxes[position[1]].classList.add("win");
+      boxes[position[2]].classList.add("win");
     }
-
-    // UI UPDATE
-    gameinfo.innerText = ` Current Player -${currentPlayer}`;
+  });
+  //  We Have A Winner
+  if (result !== "") {
+    gameInfo.innerText = `Winner Player - ${result}`;
+    newGamebtn.classList.add("active");
+    return;
+  }
+  let boardFilled = true;
+  gameGrid.forEach((box) => {
+    if (box === "") boardFilled = false;
+  });
+  // Board is filled, but game is tie
+  if (boardFilled) {
+    gameInfo.innerText = "Game Tied !";
+    newGamebtn.classList.add("active");
+    return;
+  }
 }
 
-
-function handleClick(index){
-    if(gameGrid===""){
-        boxes[index].innerText=currentPlayer;
-        gameGrid[index] = currentPlayer;  
-        boxes[index].style.pointerEvents = "none";
-        
-        // swap turn
-
-        swapTurn();
-
-        // check game over or win
-        checkGameOver();
-    }
-
-}
-
-
-
-boxes.forEach((box, index)=>{
-    box.addEventListener("click",()=>{
-        handleClick(index);
-    })
-
+// adding event listeners
+boxes.forEach((box, index) => {
+  box.addEventListener("click", () => {
+    handleClick(index);
+  });
 });
 
+initGame();
 
-
-
+newGamebtn.addEventListener("click", initGame);
