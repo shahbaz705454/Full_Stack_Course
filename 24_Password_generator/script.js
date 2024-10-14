cee865950112ed0165272b07abe37b223d425886
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-10-14 20:15:36 +0530

    addedvarible in js

--- 24_Password_generator/script.js
+++ 24_Password_generator/script.js
@@ -1,2 +1,13 @@
 const inputSlider = document.querySelector("[data-lengthSlider]");
-const 
+const lengthDisplay =document.querySelector("[data-lengthNumber]");
+const passwordDisplay =document.querySelector("[data-passwordDisplay]");
+const copyMsg =document.querySelector("[data-copyMsg]");
+const copyBtn =document.querySelector("[data-copy]");
+const uppercaseCheck =document.querySelector("#uppercase");
+const lowercaseCheck =document.querySelector("#lowercase");
+const numbersCheck =document.querySelector("#numbers");
+const symbolCheck =document.querySelector("#symbol");
+const indicator =document.querySelector("[data-indicator]");
+const generateBtn =document.querySelector(".generateBtn");
+const allCheckbox =document.querySelectorAll("input[type=checkbox]");
+
