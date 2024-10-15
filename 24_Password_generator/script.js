c48a46bde257c93eef76f1537350753f4539e023
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-10-15 16:42:12 +0530

    added new files

--- 24_Password_generator/script.js
+++ 24_Password_generator/script.js
@@ -10,4 +10,42 @@ const symbolCheck =document.querySelector("#symbol");
 const indicator =document.querySelector("[data-indicator]");
 const generateBtn =document.querySelector(".generateBtn");
 const allCheckbox =document.querySelectorAll("input[type=checkbox]");
+const symbols = '`~!@#$%^&*()_+-={[}]|;:"<,>.?/';
 
+
+let paswword = "";
+let passwordLength = 10;
+let checkCount =1;
+handleSlider();
+// set strength color to grey
+
+function handleSlider(){
+    inputSlider.value= passwordLength;
+    lengthDisplay.innerText =10;
+
+}
+
+function setIndicator(color){
+    indicator.style.backgroundColor = color;
+    //shadow
+
+}
+
+
+function getRndInteger(min,max){
+    return Math.floor(Math.random() * (min-max)) + min ;
+}
+
+ function generateRandomNumber(){
+    return getRndInteger(0,9);
+ }
+
+
+ function generateLowerCase(){
+    return String.fromCharCode(getRndInteger(97,123));
+ }
+
+ 
+ function generateUpperCase(){
+    return String.fromCharCode(getRndInteger(65,91));
+ }
