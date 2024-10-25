82736ebb19779aa7cd6126855f7f6b013396091d
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-10-25 19:03:28 +0530

    coompleted first class of react

--- 27_First_ReactApp/demoshopapp/src/components/productForm.js
+++ 27_First_ReactApp/demoshopapp/src/components/productForm.js
@@ -1,33 +1,55 @@
 import { useState } from 'react';
 import './productForm.css';
 
-function ProductForm(){
-    const [title,setTitle] = useState('');
-    const [date,setDate] =useState('');
+function ProductForm() {
+
+    // const [fullProductInout, setfullProductInput] = useState({
+    //     title:'',
+    //     date:''
+    // });
+   
 
 
-    function titleChangeHandler(event){
-        setTitle(event.target.value);
-        // console.log(event.target.value);
-        
-    }
 
-    function dateChangeHandler(event){
-        setDate(event.target.value);
-        // console.log(event.target.value);
-        console.log(title);
-        console.log(date);
 
-    }
+        const [newTitle,setTitle] = useState('');
+        const [newDate,setDate] =useState('');
 
-    return(<form className='form'>
+
+        function titleChangeHandler(event){
+            setTitle(event.target.value);
+            // console.log(event.target.value);
+
+        }
+
+        function dateChangeHandler(event){
+            setDate(event.target.value);
+            // console.log(event.target.value);
+            // console.log(title); 
+            // console.log(date);
+
+        }
+        function submitHandler(event){
+            event.preventDefault();
+
+            const productData={
+                title:newTitle,
+                date :newDate
+            };
+            console.log(productData);
+            setTitle('');
+            setDate('');
+
+        }
+
+    return (<form className='form' onSubmit={submitHandler}>
         <div className='new-Product_title'>
             <label >Title : </label>
-            <input type='text' onChange={titleChangeHandler}></input>
+            <input type='text' value={newTitle} onChange={titleChangeHandler}></input>
         </div>
         <div className='new-Product_date'>
             <label>Date : </label>
-            <input type='date' onChange={dateChangeHandler} min='2023-01-01' max='2023-12-12'></input>
+            <input type='date' value={newDate} onChange={dateChangeHandler} min='2023-01-01' max='2023-12-12'></input>
         </div>
         <div className='new-Product_button'>
             <button type='submit'>Add product</button>
