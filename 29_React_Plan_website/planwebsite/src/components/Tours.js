e200710bd849db9905025ad297be00df9305b88f
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-10-26 13:51:11 +0530

    plan website project comnpleted

---
+++ 29_React_Plan_website/planwebsite/src/components/Tours.js
@@ -0,0 +1,21 @@
+import Cards from "./Cards";
+function Tours({ tours,removeTour}) {
+    return (
+        <div className="container">
+            <div>
+                <h2 className="title">Plan With Shahbaz</h2>
+            </div>
+            <div className="cards">
+                {
+                    tours.map((tour) => {
+                        return <Cards {...tour} removeTour={removeTour}></Cards>
+                    })
+                }
+
+            </div>
+
+        </div>
+
+    );
+}
+export default Tours;
