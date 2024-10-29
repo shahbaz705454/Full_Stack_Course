8143ee89d3a4e90bfbed28b9eaf77c163aa5f725
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-10-29 12:46:24 +0530

    completed form project

--- 33_React_Form/formapp/src/App.js
+++ 33_React_Form/formapp/src/App.js
@@ -4,16 +4,16 @@ import Form from "./Form";
 
 function App() {
 
-  // const [firstName,setFirstName] = useState("");
-  // const [secondName,setSecondName] = useState("");
+  // const [firstName, setFirstName] = useState("");
+  // const [secondName, setSecondName] = useState("");
 
-  // function changeFirstHandler(event){
+  // function changeFirstHandler(event) {
   //   // console.log("printing First Name")
   //   // console.log(event.target.value);
   //   setFirstName(event.target.value)
   //   console.log(firstName)
   // }
-  // function changeSecondHandler(events){
+  // function changeSecondHandler(events) {
   //   // console.log("Printing Second Name");
   //   // console.log(event.target.value);
   //   setSecondName(events.target.value);
@@ -21,36 +21,36 @@ function App() {
   // }
 
 
-//   const [formData, SetFormData] = useState(
-//     {
-//       firstName: "",
-//       secondName: "",
-//       email: "",
-//       comment: "",
-//       checkbox: true,
-//       mode: "Online-Mode",
-//       favCar: ""
-//     }
+  // const [formData, SetFormData] = useState(
+  //   {
+  //     firstName: "",
+  //     secondName: "",
+  //     email: "",
+  //     comment: "",
+  //     checkbox: false,
+  //     mode: "Online-Mode",
+  //     favCar: ""
+  //   }
 
-//   )
-//   console.log(formData);
+  // )
+  // console.log(formData);
 
-//   function changeHandler(event) {
-//     const { name, value, checked, type } = event.target
-//     SetFormData(preFormData => {
-//       return {
-//         ...preFormData,
-//         [name]: type === "checkbox" ? checked : value
-//       }
-//     });
+  // function changeHandler(event) {
+  //   const { name, value, checked, type } = event.target
+  //   SetFormData(preFormData => {
+  //     return {
+  //       ...preFormData,
+  //       [name]: type === "checkbox" ? checked : value
+  //     }
+  //   });
 
-//   }
+  // }
 
-// function submitHandler(event){
-//   event.preventDefault();
-//   console.log("submit pr Click hua hai")
+  // function submitHandler(event) {
+  //   event.preventDefault();
+  //   console.log("submit pr Click hua hai")
 
-// }
+  // }
 
 
   return (
@@ -59,51 +59,49 @@ function App() {
       <Form></Form>
 
 
+
+
+
     </div>
 
+  
+  
+  
+  
+    //   <div>
+  //   <form className='form' onSubmit={submitHandler}>
+  //     <input type='text' placeholder='First Name' onChange={changeHandler} name="firstName" />
+  //     <br /><br />
 
+  //     <input type='text' placeholder='Second Name' onChange={changeHandler} name="secondName" />
+  //     <br /><br />
 
-  //   <div className='app'>
-  //     <form className='form' onSubmit={submitHandler}>
-  //       <input type='text' placeholder='First Name' onChange={changeHandler} name="firstName" ></input>
+  //     <input type='email' placeholder='Email' onChange={changeHandler} name="email" />
+  //     <br /><br />
 
-  //       <br></br><br></br>
+  //     <textarea placeholder="Enter your Comments" onChange={changeHandler} name="comment" value={formData.comment}></textarea>
+  //     <br /><br />
 
-  //       <input type='text' placeholder='Second Name' onChange={changeHandler} name="secondName" ></input>
-  //       <br /><br />
-  //       <input type='email' placeholder='Email' onChange={changeHandler} name="email" ></input>
+  //     {/* check box */}
 
-  //       {/* TEXT AREA */}
+  //     <input type='checkbox' onChange={changeHandler} name="checkbox" id="checkbox" checked={formData.checkbox} />
+  //     <label htmlFor="checkbox">checkbox</label>
+  //     <br /><br />
 
-  //       <br /><br />
-  //       <textarea placeholder="Enter your Comments" onChange={changeHandler} name="comment" value={formData.comment}></textarea>
+  //     <fieldset>
+  //       <legend>Mode:</legend>
+  //       <input type="radio" onChange={changeHandler} name="mode" value="Online-Mode" id="Online-Mode" checked={formData.mode === "Online-Mode"} />
+  //       <label htmlFor="Online-Mode"> Online-Mode</label>
+  //       <br />
 
-  //       {/* CHECK BOX */}
+  //       <input type="radio" onChange={changeHandler} name="mode" value="Offline-Mode" id="Offline-Mode" checked={formData.mode === "Offline-Mode"} />
+  //       <label htmlFor="Offline-Mode"> Offline-Mode</label>
+  //     </fieldset>
+  //     <br />
 
-  //       <br /><br />
-  //       <input type='checkbox' placeholder='Email' onChange={changeHandler} name="checkbox" id="checkbox" checked={formData.checkbox} />
-  //       <label htmlFor="checkbox">checkbox</label>
-
-  //       <br /><br />
-
-  //       {/* RADIO BUTTON */}
-  //       <fieldset>
-  //         <legend>Mode : </legend>
-
-  //         <input type="radio" onChange={changeHandler} name="mode" value="Online-Mode" id="Online-Mode" checked={formData.mode === "Online-Mode"}></input>
-  //         <label htmlFor="Online-Mode"> Online-Mode</label>
-  //         <br></br>
-
-  //         <input type="radio" onChange={changeHandler} name="mode" value="Offline-Mode" id="Offline-Mode" checked={formData.mode === "Offline-Mode"}></input>
-  //         <label htmlFor="Offline-Mode"> Offline-Mode</label>
-  //       </fieldset>
-  //       <br></br>
-
-  //       {/* DROP DOWN */}
-
-  //       <label>Tell me Your Car </label>
-  //       <select onChange={changeHandler} name="favCar" id="favCar" value={formData.favCar} >
-
+  //     <label>Tell me Your Car:</label>
+  //     <select onChange={changeHandler} name="favCar" id="favCar" value={formData.favCar}>
+  //       <option value="">Select your car</option>
   //       <option value="scorpio"> Scorpio</option>
   //       <option value="Thor"> Thor</option>
   //       <option value="Kia"> Kia</option>
@@ -117,28 +115,14 @@ function App() {
   //       <option value="Mercedies"> Mercedies</option>
   //       <option value="Audi"> Audi</option>
   //       <option value="Mustang"> Mustang</option>
+  //     </select>
 
-  //       </select>
-
-  //       {/* Button */}
-
-  //       <br/><br/>
-
-
-  //       {/* <input type='submit' value='submit'/>  */}
-
-
-  //       <button >Submit</button>
-
-
-  //       {/* Button */}
-
-       
-
-
-
-
-  );
+  //     <br /><br />
+  //     <button type="submit">Submit</button>
+  //   </form>
+  // </div>
+);
+  
 }
 
 export default App;
--- 33_React_Form/formapp/src/Form.js
+++ 33_React_Form/formapp/src/Form.js
@@ -3,54 +3,194 @@ import React, { useState } from 'react'
 const Form = () => {
 
     const [formData, setFormData] = useState({
-        firstName:"",
-        lastName:"",
-        email:"",
-        country:"",
-        Street:"",
-        city:"",
-        state:"",
-        zip:"",
+        firstName: "",
+        lastName: "",
+        email: "",
+        country: "",
+        street: "",
+        city: "",
+        state: "",
+        zip: "",
+        comment: false,
+        candidate: false,
+        offers: false,
+        pushNotifications: ""
+
 
 
     });
 
-    function clickHandler(event){
-        const {name,type,checked,value} = event.target;
+    // console.log(formData);
 
-        setFormData((prev)=>{
-            return{
+    function clickHandler(event) {
+        const { name, type, checked, value } = event.target;
+
+        setFormData((prev) => {
+            return {
                 ...prev,
-                [name]:type==="checkbox"?checked:value
+                [name]: type === "checkbox" ? checked : value
             }
 
         });
 
-        
+        // console.log(formData);
+
+
 
 
     }
-  return (
-    <div className='w-full md:max-w-[50%] mx-auto shadow p-8'>
 
-        <form className=' flex flex-col gap-2  mx-auto justify-center mt-4 '>
-            <label>First Name</label>
-            <input onClick={clickHandler} className='border-2  border-black'></input>
-            
+    function submitHandler(event) {
+        event.preventDefault();
+        // console.log("submit pr Click hua hai")
+        console.log(formData);
+
+    }
+
+    return (
+        <div className='w-full md:max-w-[50%] mx-auto shadow p-8'>
+
+            <form className=' flex flex-col gap-2  mt-4 main'>
+                <label htmlFor='firstName'>First Name</label>
+                <input className='basic' onChange={clickHandler} type='text'
+                    name='firstName' value={formData.firstName} id='firstName' placeholder='Mohd'
+                />
+
+
+                <label htmlFor='lastName'>Last Name</label>
+                <input className='basic' onChange={clickHandler} type='text'
+                    name='lastName' value={formData.lastName} id='lastName' placeholder='Shahbaz'
+                />
+
+                <label htmlFor='email'>Email Address</label>
+                <input className='basic' onChange={clickHandler} type='email'
+                    name='email' value={formData.email} id='email' placeholder='Shahbaz454@gmail.com'
+                />
+
+                <label htmlFor='country'>Country</label>
+                <select onChange={clickHandler} name='country' id='country' value={formData.value}>
+                    <option value='India'>India</option>
+                    <option value='Pakistan'>Pakistan</option>
+                    <option value='United State'>United State</option>
+                    <option value='Canada'>Canada</option>
+                    <option value='Germany'>Germany</option>
+                    <option value='Russian'>Russia</option>
+                </select>
+
+                <label htmlFor='street'>Street Address</label>
+                <input className='basic' onChange={clickHandler} type='street'
+                    name='street' value={formData.street} id='street' placeholder='1234 choti gali'
+                />
+
+                <label htmlFor='city'>City</label>
+                <input className='basic' onChange={clickHandler} type='city'
+                    name='city' value={formData.city} id='city' placeholder='Manali'
+                />
+
+                <label htmlFor='state'>State</label>
+                <input className='basic' onChange={clickHandler} type='state'
+                    name='state' value={formData.state} id='state' placeholder='UK'
+                />
+
+                <label htmlFor='zip'>Zip Code</label>
+                <input className='basic' onChange={clickHandler} type='zip'
+                    name='zip' value={formData.zip} id='zip' placeholder='112244'
+
+                />
+
+                {/* checkBox */}
+
+
+                <fieldset className='mt-2' >
+                    <legend className='font-semibold'>
+                        By Email
+                    </legend>
+                    <div className='flex mt-5 gap-2 '>
+                        <input className='checkbox' id='comments' type="checkbox" onChange={clickHandler} name='comment' checked={formData.comment} />
+                        <div>
+                            <label htmlFor='comments'>Comments</label>
+                            <p className=' opacity-60 '>Get notified when someones posts a comment on a posting.</p>
+                        </div>
+                    </div>
+
+
+                    <div className='flex mt-5 gap-2 '>
+                        <input className='checkbox' id='candidate' type="checkbox" onChange={clickHandler} name='candidate' checked={formData.candidate} />
+                        <div>
+                            <label htmlFor='candidate'>Candidates</label>
+                            <p className=' opacity-60 '>Get notified when a candidate applies for a job.</p>
+                        </div>
+                    </div>
+
+                    <div className='flex mt-5 gap-2 '>
+                        <input className='checkbox' id='offers' type="checkbox" onChange={clickHandler} name='offers' checked={formData.offers} />
+                        <div>
+                            <label htmlFor='offers'>Offers</label>
+                            <p className=' opacity-60 '>Get notified when a candidate accepts or rejects an offer.</p>
+                        </div>
+                    </div>
+
+
+
+                </fieldset>
+
+
+
+                <fieldset className='flex  flex-col gap-4'>
+                    <legend className='font-semibold'>
+                        Push Notifications
+                    </legend>
+                    <p className='font-extralight text-sm opacity-60'>These are delivered via SMS to your mobile phones.</p>
+
+                    <div className='flex gap-2'>
+                        <input type='radio' id='everything' name='pushNotifications' value="everything" onChange={clickHandler}></input>
+                        <label htmlFor='everything'>Everything</label>
+                    </div>
+
+                    <div className='flex gap-2'>
+
+
+
+                        <input className='' type='radio' id='same' name='pushNotifications' value="Same as email" onChange={clickHandler}></input>
+                        <label htmlFor='same'>Same as Email</label>
+
+                    </div>
+
+
+                    <div className='flex gap-2'>
+                        <input type='radio' id='push' name='pushNotifications' value="No Push Notifications" onChange={clickHandler}></input>
+                        <label htmlFor='push'>No Push Notifications</label>
+                    </div>
+
+
+                </fieldset>
+                <div className='mt-3'>
+                    <button onClick={submitHandler}
+                        className='border-black bg-blue-500 py-2 px-4 rounded-md text-white hover:bg-blue-700' >Save</button>
+
+                </div>
 
 
 
 
 
-        </form>
 
 
 
 
 
 
-    </div>
-  )
+
+
+            </form>
+
+
+
+
+
+
+        </div>
+    )
 }
 
 export default Form
