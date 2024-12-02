1055d14e7205beb15791c89551a5e62616651396
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2024-12-02 21:10:33 +0530

    completetd event

--- 0_JavaScript/6_DOM_manipulation.js
+++ 0_JavaScript/6_DOM_manipulation.js
@@ -1,4 +1,11 @@
 let text = document.querySelector("#heading");
+let countBox = document.querySelector("#countBox");
+let btnP = document.querySelector("#btnP")
+let btnM = document.querySelector("#btnM")
+let body = document.querySelector("body")
+let mode = document.querySelector("#mode")
+let currMode ="light";
+body.classList.add(currMode);
 
 function changeHandler(){
 
@@ -8,8 +15,64 @@ function changeHandler(){
                 text.innerHTML = "This is original";
             }
 }
+let count =0;
 
-console.log(text.innerText);
+// const plusHandler=(()=>{
+    
+//     count++;
+//     countBox.innerText = count;
+//     console.log(count)
+    
+
+// })
+
+// const minHandler=((e)=>{
+//     count--;
+//     countBox.innerText = count;
+//     console.log(count)
+//     console.log(e)
+
+// })
+// // console.log(countBox)
+
+
+
+btnP.addEventListener('click', ((e)=>{
+    count++;
+    countBox.innerText = count;
+    console.log(e)
+
+
+}))
+
+
+btnM.addEventListener('click', ((e)=>{
+    count--;
+    countBox.innerText = count;
+    console.log(e)
+
+
+}))
+
+// btnP.removeEventListener("click",)
+
+mode.addEventListener('click',(()=>{
+    if(currMode ==="light"){
+        currMode="dark";
+        body.classList.add("light");
+        body.classList.remove("dark")
+
+    }else{
+        currMode="light";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }
+}))
+
+console.log(body)
+
+
+
 
 
 
